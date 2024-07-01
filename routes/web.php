105d84b2fb2ba17064aae052aea671868cb099a5
Tom Abella <tomabella64@gmail.com>
2024-07-01 10:14:22 +0800

    UPDATE
    * Added a middleware to restrict super admin access.

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\MyProfile;
 use App\Livewire\Rooms;
 use App\Livewire\StudentSchedule;
 use App\Livewire\Users;
+use App\Http\Middleware\SuperAdminAccess;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,9 +29,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['auth', 'superadmin']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::get('/rooms', Rooms::class)->name('rooms');
     //// Route::get('/faculty', Faculty::class)->name('faculty');
     Route::get('/courses', Course::class)->name('courses');
@@ -38,4 +38,8 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/student-schedules', StudentSchedule::class)->name('student-schedules');
     Route::get('/users', Users::class)->name('users');
     Route::get('/profile', MyProfile::class)->name('profile');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
